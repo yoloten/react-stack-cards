a2bfca300426a9907ff196b2afc8a86d94b13676
rustam1998 <rustamsahatov50@gmail.com>
2019-06-12 14:46:39 +0300

    tinderlike animation v0.5

--- src/TinderLikeCard.tsx
+++ src/TinderLikeCard.tsx
@@ -19,27 +19,72 @@ namespace TinderLike {
 
 const Li = posed.li({
     pressable: true,
-    enter: {
-        y: ({ order }: TinderLike.Props): number => {
+    init: {
+        y: ({ order }: TinderLike.Props) => {
             return objectSwitch(order, {
                 0: 0,
-                1: 15,
-                2: 30,
+                1: 25,
+                2: 50,
             })
         },
-        x: 0,
-        opacity: 1,
+        scale: ({ order }: TinderLike.Props) => {
+            return objectSwitch(order, {
+                0: 0.9,
+                1: 0.95,
+                2: 1,
+            })
+        },
+    },
+    1: {
+        y: ({ order }: TinderLike.Props) => {
+            return objectSwitch(order, {
+                0: 25,
+                1: 50,
+                2: -1,
+            })
+        },
+        scale: ({ order }: TinderLike.Props) => {
+            return objectSwitch(order, {
+                0: 0.95,
+                1: 1,
+                2: 0.9,
+            })
+        },
+    },
+    2: {
+        y: ({ order }: TinderLike.Props) => {
+            return objectSwitch(order, {
+                0: 50,
+                1: -1,
+                2: 25,
+            })
+        },
+        scale: ({ order }: TinderLike.Props) => {
+            return objectSwitch(order, {
+                0: 1,
+                1: 0.9,
+                2: 0.95,
+            })
+        },
+    },
+    3: {
+        y: ({ order }: TinderLike.Props) => {
+            return objectSwitch(order, {
+                0: -1,
+                1: 25,
+                2: 50,
+            })
+        },
+        scale: ({ order }: TinderLike.Props) => {
+            return objectSwitch(order, {
+                0: 0.9,
+                1: 0.95,
+                2: 1,
+            })
+        },
     },
     press: {
-        y: 0,
-        scale: 1,
-        opacity: 0.001,
-        transition: {duration: 700}
-    },
-    exit: {
-        y: 100,
-        x: 0,
-        opacity: 0.01,
+        transition: { duration: 500 },
     },
 })
 
@@ -56,50 +101,52 @@ const StyledLi = styled(Li)`
   top: 0;
   left: 0;
   background: ${({ color }: TinderLike.Props): string => color};
-  z-index: ${({ order, pose }) => order === 0 && pose === "afterclick" ? -100 : 1};
-`  
+`
 const Button = styled.button`
   margin-top: 20rem
 `
 
-export class TinderLikeCard extends React.Component <{}, {}> {
+export class TinderLikeCard extends React.Component<{}, {}> {
 
     public state = {
         items: [0, 1, 2],
+        clickTimes: 0,
     }
-    
-    click = () => {
+
+    public click = () => {
         const array = this.state.items
         array.pop()
+        this.setState({ items: array })
         this.setState({
-            items: array,
+            clickTimes: this.state.clickTimes < 3
+                ? this.state.clickTimes += 1
+                : this.state.clickTimes - 2,
         })
-        setTimeout(() => {
-            if (array.length < 3) {
-                if (array[0] === 2) {
-                    array.unshift(1)
-                }
-                if (array[0] === 1) {
-                    array.unshift(0)  
-                } else {
-                    array.unshift(2)
-                }
-            } 
-        }, 100) 
+
+        if (array.length < 3) {
+            if (array[0] === 2) {
+                array.unshift(1)
+            } else if (array[0] === 1) {
+                array.unshift(0)
+            } else {
+                array.unshift(2)
+            }
+        }
+
     }
 
     public render() {
         const array = this.state.items
-        console.log(array)
+        //console.log(array)
+        const pose = this.state.clickTimes
+        //console.log(pose)
         const colors: string[] = ["#4286f4", "#e2f442", "#86f441"].reverse()
         return (
             <StyledUl>
-                <PoseGroup>
-                    {array.map((i) => (
-                        <StyledLi onClick={this.click} order={i} key={i} color={colors[i]} />
-                    ))
-                    }
-                </PoseGroup>
+                {array.map((i) => (
+                    <StyledLi pose={pose} onClick={this.click} order={i} key={i} color={colors[i]} />
+                ))
+                }
             </StyledUl>
         )
     }
