39a2af7d2fd8c0785d9a0d1eb90f0c34c0f6eb1a
rustam1998 <rustamsahatov50@gmail.com>
2019-06-18 22:15:39 +0300

    tinderlike animation almost done

--- src/TinderLikeCard.tsx
+++ src/TinderLikeCard.tsx
@@ -26,17 +26,53 @@ const Li = posed.li({
     init: {
         opacity: 1,
         x: 0,
-        y: 1,
+        y: ({ order }: TinderLike.Props): number => {
+            return objectSwitch(order, {
+                0: 60,
+                1: 40,
+                2: 20,
+                3: 20,
+                4: 10,
+            })
+        },
+        scale: ({ order }: TinderLike.Props): number => {
+            return objectSwitch(order, {
+                0: 1,
+                1: 0.95,
+                2: 0.9,
+            })
+        },
         rotate: 0,
-        transition: { duration: 250},
-      },
-      out: {
+        transition: { duration: 250 },
+    },
+    out: {
         opacity: 0,
         x: -25,
         y: 800,
         rotate: -225,
-        transition: { duration: 500},
-      },
+        transition: { duration: 500 },
+    },
+    middle: {
+        opacity: 1,
+        x: 0,
+        y: 60,
+        transition: { duration: 500 },
+        scale: 1,
+    },
+    secondMiddle: {
+        opacity: 1,
+        x: 0,
+        y: 40,
+        transition: { duration: 500 },
+        scale: 0.95,
+    },
+    in: {
+        opacity: 1,
+        x: 0,
+        y: 20,
+        transition: { duration: 500 },
+        scale: 0.90,
+    },
 })
 
 const StyledUl = styled.ul`
@@ -68,17 +104,18 @@ class TinderLikeCard extends React.Component<TinderLike.Props, TinderLike.State>
 
     public componentDidMount() {
         if (this.state.list.length === 0) {
-        const newList = this.state.list
-        for (let i = 0; i < 3; i++) {
-            newList.push({
-                val: this.props.content[i],
-                out: "",
-                in: "",
-            })
-        }
-        const newContent = this.props.content.slice(3)
-        this.setState({ list: newList })
-        this.setState({ content: newContent })
+            const newList = this.state.list
+            for (let i = 0; i < 3; i++) {
+                newList.push({
+                    val: this.props.content[i],
+                    out: "",
+                    in: "",
+                    middle: "",
+                })
+            }
+            const newContent = this.props.content.slice(3)
+            this.setState({ list: newList })
+            this.setState({ content: newContent })
         }
     }
 
@@ -86,16 +123,21 @@ class TinderLikeCard extends React.Component<TinderLike.Props, TinderLike.State>
         const newList = this.state.list
         const currentContent = this.state.content
         newList.push({
-                val: currentContent[0],
-                out: "",
-                in: "in",
-            })
-    
-        this.setState({list: newList})
+            val: currentContent[0],
+            out: "",
+            in: "in",
+            middle: "",
+        })
+        newList[this.state.current].out = "out"
+        if (newList[this.state.current].out === "out") {
+            newList[this.state.current].middle = ""
+            newList[this.state.current + 1].middle = "middle"
+            newList[this.state.current + 2].middle = "secondMiddle"
+        }
+        newList[this.state.list.length - 2].in = ""
+        this.setState({ list: newList })
         this.setState({ content: currentContent.slice(1) })
-        this.state.list[this.state.current].out = "out"
-        this.state.list[this.state.list.length - 2].in = ""
-        this.setState({current: this.state.current += 1 })
+        this.setState({ current: this.state.current += 1 })
     }
 
     public render() {
@@ -105,12 +147,12 @@ class TinderLikeCard extends React.Component<TinderLike.Props, TinderLike.State>
             <StyledLi
                 color={obj.val}
                 key={key}
-                pose={obj.out}
-            />
+                pose={[obj.out, obj.middle, obj.in]}
+                order={key}
+            >Lol</StyledLi>
         )).reverse() : ""
         // const t1 = performance.now()
         // console.log("Call to doSomething took " + (t1 - t0) / 1000 + " seconds. " + list.length)
-       
         // console.log(list)
         return (
             <StyledUl>
