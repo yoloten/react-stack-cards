ba3700d4c99d0331a5bccf1cdd584943f6bcdcc6
rustam1998 <rustamsahatov50@gmail.com>
2019-06-30 21:19:45 +0300

    ts support

---
+++ src/index.d.ts
@@ -0,0 +1,50 @@
+declare module "react-stack-cards" {
+    export interface Stack {
+        children: React.ReactNode
+        onMouseEnter: () => void
+        onMouseLeave: () => void
+        [propName: string]: any
+        onClick: () => void
+        className: string
+        direction: string
+        duration: number
+        images: string[]
+        height: number
+        color: string
+        width: number
+    }
+
+    export interface TinderLike {
+        children: React.ReactNode
+        [propName: string]: any
+        className: string
+        direction: string
+        duration: number
+        colors: string[]
+        images: string[]
+        height: string
+        width: string
+    }
+
+    export interface Toggle {
+        children: React.ReactNode
+        onMouseEnter: () => void
+        onMouseLeave: () => void
+        [propName: string]: any
+        onClick: () => void
+        className: string
+        direction: string
+        duration: number
+        colors: string[]
+        images: string[]
+        height: string
+        width: string
+    }
+
+    export class StackCard extends React.Component <Stack, any> {
+    }
+    export class TinderLikeCard extends React.Component<TinderLike, any> {
+    }
+    export class ToggleCard extends React.Component<Toggle, any> {
+ } 
+}
